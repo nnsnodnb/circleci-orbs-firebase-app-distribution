59384fc7cd7327dbd69800b5908c1c3f582f46d6
Yuya Oka <nnsnodnb@gmail.com>
2022-11-17 19:19:43 +0900

    Fix SC2128

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
+#!/bin/bash -u
 
 cmd="firebase appdistribution:distribute ${BINARY_PATH}"
-if [[ -n ${TOKEN} ]]; then
+if [[ -n "${TOKEN}" ]]; then
     cmd="${cmd} --token ${TOKEN}"
 fi
-if [[ -n ${RELEASE_NOTE} ]]; then
+if [[ -n "${RELEASE_NOTE}" ]]; then
     cmd="${cmd} --release-note ${RELEASE_NOTE}"
 fi
-if [[ -n ${RELEASE_NOTE_FILE} ]]; then
+if [[ -n "${RELEASE_NOTE_FILE}" ]]; then
     cmd="${cmd} --release-note-file ${RELEASE_NOTE_FILE}"
 fi
-if [[ -n ${TESTERS} ]]; then
-    cmd="${cmd} --testers ${TESTERS}"
+if [[ -n "${TESTER}" ]]; then
+    cmd="${cmd} --testers ${TESTER}"
 fi
-if [[ -n ${TESTERS_FILE} ]]; then
-    cmd="${cmd} --testers_file ${TESTERS_FILE}"
+if [[ -n "${TESTER_FILE}" ]]; then
+    cmd="${cmd} --testers_file ${TESTER_FILE}"
 fi
-if [[ -n ${GROUPS} ]]; then
-    cmd="${cmd} --groups ${GROUPS}"
+if [[ -n "${GROUP}" ]]; then
+    cmd="${cmd} --groups ${GROUP}"
 fi
-if [[ -n ${GROUPS_FILE} ]]; then
-    cmd="${cmd} --groups_file ${GROUPS_FILE}"
+if [[ -n "${GROUP_FILE}" ]]; then
+    cmd="${cmd} --groups_file ${GROUP_FILE}"
 fi
 if [[ "${DEBUG}" = true ]]; then
     cmd="${cmd} --debug"
