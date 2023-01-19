12a521b2e62b70ad37b75dfd8494045b4f2769ac
Yuya Oka <nnsnodnb@gmail.com>
2023-01-19 19:14:04 +0900

    Escape quotes

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -18,19 +18,19 @@ if [[ -n "${RELEASE_NOTE}" ]]; then
     cmd="${cmd} --release-notes \"${escaped_release_notes}\""
 fi
 if [[ -n "${RELEASE_NOTE_FILE}" ]]; then
-    cmd="${cmd} --release-notes-file ${RELEASE_NOTE_FILE}"
+    cmd="${cmd} --release-notes-file \"${RELEASE_NOTE_FILE}\""
 fi
 if [[ -n "${TESTER}" ]]; then
-    cmd="${cmd} --testers ${TESTER}"
+    cmd="${cmd} --testers \"${TESTER}\""
 fi
 if [[ -n "${TESTER_FILE}" ]]; then
-    cmd="${cmd} --testers-file ${TESTER_FILE}"
+    cmd="${cmd} --testers-file \"${TESTER_FILE}\""
 fi
 if [[ -n "${GROUP}" ]]; then
-    cmd="${cmd} --groups ${GROUP}"
+    cmd="${cmd} --groups \"${GROUP}\""
 fi
 if [[ -n "${GROUP_FILE}" ]]; then
-    cmd="${cmd} --groups-file ${GROUP_FILE}"
+    cmd="${cmd} --groups-file \"${GROUP_FILE}\""
 fi
 if [[ "${DEBUG}" = "true" ]]; then
     cmd="${cmd} --debug"
